d2affa34afb7233b63409d210cc3d36c6742ad0e
Lazyuno1987 <95475727+Lazyuno1987@users.noreply.github.com>
2022-11-02 18:29:09 +0300

    end

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL:'http://localhost:4444'
+    baseURL:process.env.REACT_API_API_URL
 })
 
 instance.interceptors.request.use((config) => {
--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -57,7 +57,7 @@ const dispatch = useDispatch()
       {imageUrl && (
         <img
           className={clsx(styles.image, { [styles.imageFull]: isFullPost })}
-          src={`http://localhost:4444/${imageUrl}`}
+          src={`${process.env.REACT_API_API_URL}${imageUrl}`}
           alt={title}
         />
       )}
